828e711a8fee3245f975cb619a13eab39dca137f
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-11-27 20:47:10 -0500

    Add variables to select color blocks, change white class to transparent

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// variables to select color
+
+const colorName = document.getElementsByTagName('p');
+const colorBlock = document.getElementsByClassName('square');
+
+
+function wrapper() {
+
+  const flashColor = () => {
+    colorBlock.classList.toggle('transparent');
+  };
+
+  const loopThrough = () => {
+    let colorBlocksText = this.childNodes[0].nodeValue;
+    let BlocksTextLength = colorBlocksText.length;
+
+    for (let i = 0; i < BlocksTextLength * 2; i++) {
+      setTimeout(function(){
+        flashColor();
+      }, i * 1000);
+    };
+
+  };
+  loopThrough();
+};
+
+for(let i = 0; i < colorName.length; i++) {
+  colorName[i].addEventListener('click', wrapper);
+};
